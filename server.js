553de0f8c7b10f576f8f2a670701eef65c8fc1e7
paramjeet <parmjeetchauhan24@gmail.com>
2026-03-27 01:25:16 +0530

    jai ho

--- server.js
+++ server.js
@@ -51,7 +51,15 @@ const corsOptions = {
 // app.post('/api/stripe-webhook', express.raw({ type: 'application/json' }), stripeWebhook);
 
 app.use(cors(corsOptions));
-app.options('/*', cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, access_token");
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
