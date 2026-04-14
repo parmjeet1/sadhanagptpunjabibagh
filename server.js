658cb0efe366f33e26293e81218d4057c5b851b3
paramjeet <parmjeetchauhan24@gmail.com>
2026-04-14 10:43:47 +0530

    remove dist from git and add to gitignore

--- server.js
+++ server.js
@@ -94,11 +94,10 @@
 
   app.use('/api', Routes);
 
+  app.use(express.static('/home/ubuntu/dist'));
 
-  app.use(express.static(path.join(__dirname, '..', 'dist')));
-
-  app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile('/home/ubuntu/dist/index.html');
   });
   app.use(errorHandler);
   const server = http.createServer(app);
